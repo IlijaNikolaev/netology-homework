d39df317d36595378a99db1082f4746e20e9d18a
ilija_nikolaev <nikolaev.xo@gmail.com>
2021-05-05 21:48:47 +0400

    add class-based component ShopItemFunc with PropTypes

--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,25 +1,31 @@
-import './main.css'
+import React from "react";
 import PropTypes from 'prop-types'
 
-const ShopItemFunc = (props) => {
-  const {item} = props
-  return (
-    <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className="description">{item.descriptionFull}</div>
-      <div className="highlight-window mobile">
-        <div className="highlight-overlay"></div>
+import './main.css'
+
+class ShopItemFunc extends React.Component {
+
+  render() {
+    const {item} = this.props
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
       </div>
-      <div className="divider"></div>
-      <div className="purchase-info">
-        <div className="price">{item.currency}{item.price}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
 ShopItemFunc.propTypes = {
   item: PropTypes.object
 }
